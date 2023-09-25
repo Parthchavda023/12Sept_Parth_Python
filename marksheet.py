1b6c0198719e3180d2dea1fa5ef659ab7ab55a5c
Parthchavda023 <chavdaparth023@gmail.com>
2023-09-25 11:51:36 +0530

    Marksheet

--- marksheet.py
+++ marksheet.py
@@ -9,13 +9,13 @@ print("Total Marks=",total)
 per=total/4
 print("Percenage=",per)
 
-if 70<per:
+if 70<=per:
     print("Result= Dis")
-elif 60<per:
+elif 60<=per:
     print("Result=First Class")
-elif 50<per:
+elif 50<=per:
     print("Result=Second Class")
-elif 40<per:
+elif 40<=per:
     print("Result=Pass")
 else :
     print("Result=Fail")
