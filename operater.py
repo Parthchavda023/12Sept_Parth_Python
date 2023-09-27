2a625ead395ca946235f479af2da41e37610fb45
Parthchavda023 <chavdaparth023@gmail.com>
2023-09-27 10:54:09 +0530

    first commit

---
+++ operater.py
@@ -0,0 +1,28 @@
+print("\n========1=======\n")
+s=int(input("Enter First Number:"))
+p=int(input("Enter Second Number:"))
+
+print("Add:",s+p)
+print("Sub:",s-p)
+print("Mul:",s*p)
+print("Div:",s/p)
+
+print("\n========2=======\n")
+s=16 
+print("S=",s)   #assignment operater
+p=5 
+print("P=",p)   #assignment operater
+
+print("\n========3=======\n")
+
+s+=12 
+print("S=",s)   #answer  s=s+12 :- 28=16+12
+p+=12 
+print("P=",p)   #answer  p=p+12 :- 17=5+12
+
+print("\n========4=======\n")
+
+s-=10 
+print("S=",s)   #Answer  s=s-10 :- 15
+p-=10 
+print("P=",p)   #Answer  p=p-10
