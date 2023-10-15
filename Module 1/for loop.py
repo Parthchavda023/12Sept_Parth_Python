6a1b7c018245b01238e0c5267b41400272521937
Parthchavda023 <chavdaparth023@gmail.com>
2023-10-16 02:00:17 +0530

    New

--- Module 1/for loop.py
+++ Module 1/for loop.py
@@ -1,4 +1,9 @@
 3 #Even Number
 
-for i in range(2,11,2):
+# for i in range(2,11,2):
+#     print(i)
+
+i=10
+while i>=1:
     print(i)
+    i-=1
--- Python Asssignment/Remove last obj.py
+++
@@ -1,9 +0,0 @@
-'''How will you remove last object from a list?
-Suppose list1 is [2, 33, 222, 14, and 25], what is list1 [-1]?'''
-
-
-p=[2, 33, 222, 14, 25]
-print(p)
-print("Last Obj.:",p[-1])
-p.pop(-1)
-print(p)
