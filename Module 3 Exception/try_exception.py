2bd8cfd33605aa08c204a3d49250f9a71899f32a
Parthchavda023 <chavdaparth023@gmail.com>
2023-10-27 22:28:06 +0530

    Hey

--- Module 3 Exception/try_exception.py
+++ Module 3 Exception/try_exception.py
@@ -1,6 +1,7 @@
 try:
     id=int(input("Enter Id:"))
     nm=str(input('Enter Name:'))
-    print(f'{ID}-{nm}')
+    print(f'{id}-{nm}')
+    # print(f'{ID}-{nm}')
 except Exception as p:
     print(p)
